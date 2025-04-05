ec62c677c098437a2a6290d39dd49a44df108719
Marissa Eadlinna Binti Haji Noorali <23ftt1130@student.pb.edu.bn>
2025-04-05 23:37:16 +0800

    Updated pb logo in mailtrap

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="pblogo.png" alt="PB Logo">
+<img src="https://marissaeadlinna.duckdns.org/pblogo.png" alt="PB Logo" style="height: 60px;">
 @else
 {{ $slot }}
 @endif
